5cb2a116ccb6da6808ce80f7e5cb6ad7ff770110
sampurna simkhada <sampurnasimkhada@gmail.com>
2023-11-07 16:49:13 +0545

    week1

---
+++ week 1/Sampurna_Simkhada_E1.py
@@ -0,0 +1,199 @@
+
+What is the name of the programming language that we will be using on this module? What version of the language are we using?
+Answer:
+
+Python version 3.11
+
+_____________________________________________________________________
+A computer program takes some input, performs some processing then…. what?
+Answer:
+
+Output
+
+_________________________________________________________________________
+What generation of programming language is machine code?
+Answer:
+
+First Generation
+
+_________________________________________________________________________
+Which of the following is known as a second generation programming language?
+●	C++
+●	Java
+●	Assembly
+●	R
+●	Python
+Answer:
+
+Assembly
+
+
+
+
+_________________________________________________________________________
+State one problem associated with writing code in Assembly Language.
+Answer:
+
+Complexity
+
+_________________________________________________________________________
+What generation of programming language is Python?
+Answer:
+Third Generation
+
+
+_________________________________________________________________________
+What is the purpose of a compiler?
+Answer:
+Compiler’s job is to translate high level programming code into lower level langaunge.
+
+
+_________________________________________________________________________
+The Python interpreter uses an interaction model called REPL. What does this stand for?
+Answer:
+
+REPL stands for Read-Eval-Print Loop.
+It's an interactive mode that allows users to enter and execute commands one at a time.
+
+_________________________________________________________________________
+Is it true that Python development always has to take place using interactive-mode within the Python interpreter?
+Answer:
+No, Python development does not necessarily have to take place within the Python interpreter in interactive mode.
+
+
+_________________________________________________________________________
+What does the term IDE stand for?
+Answer:
+
+Integrated Development Environment
+
+_________________________________________________________________________
+What is the main reason why programmers use code libraries?
+Answer:
+They use it for code reusability, time efficiency, access to specialized features ,quality and reliability and many more.
+
+
+_________________________________________________________________________
+The Python language is often used in the field of data-science. What other language specifically supports data-science?
+Answer:
+
+SQL, Java, R
+
+_________________________________________________________________________
+An expression within a programming language consists of operands and operators.
+Given an expression such as:  20 + 10, which part of this is the operator?
+Answer:
+
+Addition “+”
+And, which part of this is the operand?
+Answer:
+
+The numbers “20” and “10”
+_________________________________________________________________________
+Within Python, what calculation is performed by the ‘*’ operator?
+Answer:
+
+Multiplication
+And, what calculation is performed by the ‘/’ operator?
+Answer:
+Division
+
+And, what calculation is performed by the ‘**’ operator?
+Answer:
+Exponentiation
+
+_________________________________________________________________________
+Using the information about expression evaluation provided in the related tutorial, evaluate each of the following expressions in your head and type the result in the answer boxes below. Remember that an operator precedence is applied, but can be overridden by the use of parentheses.
+
+a) 100 + 200 - 50
+Answer:
+
+250
+
+
+b) 10 + 20 * 10
+
+Answer:
+
+210
+
+
+c) 20 % 3
+
+Answer:
+2
+
+d) 20 / (2 * 5)
+Answer:
+2
+
+e) 20 / 2 * 5
+Answer:
+
+50
+
+
+F) 10 * 2 + 1 * 3
+
+Answer:
+23
+
+
+
+G) 5 + 10 ** 2
+
+Answer:
+105
+
+
+
+h) (10 + 2 / 2) + ((10 * 2) ** 2)
+Answer:
+
+411
+
+_________________________________________________________________________
+Use the Python interpreter to input and then execute a simple Python expression that adds the three numbers 100.6, 200.72 and 213.3, then write the result in the answer box below.
+Answer:
+Add_num=100.6+200.72+213.3
+Print (Add_num)
+# the sum is 514.62
+_________________________________________________________________________
+Use the Python interpreter to input and then execute a simple Python expression that multiplies the three numbers 20.25, 100 and 23.9, then write the result in the answer box below.
+Answer:
+multiply_num=20.25*100*23.9
+Print (multiply_num)
+# the result is 48,397.5
+
+_________________________________________________________________________
+Use the Python interpreter to input and then execute a simple Python expression that divides the number 10 by 0, then write the result in the answer box below.
+Answer:
+divide_num=10/0
+Print (divide_num)
+# the result is not divisible
+#zerodivisionerror
+
+
+
+_________________________________________________________________________
+What type of error is typically easier to identify? A syntax error? Or a logical error?
+Answer:
+
+A syntax error
+
+_________________________________________________________________________
+What type of message is used by the Python interpreter to report run-time errors?
+Answer:
+Exceptions
+
+
+_________________________________________________________________________
+What command can be used to exit the Python interpreter?
+Answer:
+
+Exit ()
+Quit ()
+
+
+_________________________________________________________________________
+
